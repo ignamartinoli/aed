4e6e4027694ab13bd72a46b87e304adbe1b86a91
igna_martinoli <ignamartinoli@protonmail.com>
2025-02-05 12:56:05 -0300

    sisisi

--- P2/posiciones.py
+++ P2/posiciones.py
@@ -1,21 +1,33 @@
-cadena = 'Vivo en america.'  # input("Ingrese una cadena: ")
+def es_vocal(caracter):
+    return caracter in 'aeiouAEIOU'
+
+
+cadena = 'Tengo treintaytres anios.'  # input("Ingrese una cadena: ")
 
 cantidad_palabras_empiezan_terminan_igual = 0
+cantidad_palabras_con_tr_y_vocal_primeras_3 = 0
 
 # NO TE OLVIDES DE RESETEAR
 contador = 0
 primera_letra_de_palabra = None
-caracter_anterior = None
+caracter_anterior = ''
+contiene_tr = False
+incluye_vocal_en_primeras_tres = False
 
 for caracter in cadena:
-    if caracter == ' ' or caracter == '.':
+    if caracter in ' .':
         # Afuera de una palabra
         if primera_letra_de_palabra == caracter_anterior:
             cantidad_palabras_empiezan_terminan_igual += 1
 
+        if contiene_tr and incluye_vocal_en_primeras_tres:
+            cantidad_palabras_con_tr_y_vocal_primeras_3 += 1
+
         # RESET
         contador = 0
         primera_letra_de_palabra = None
+        contiene_tr = False
+        incluye_vocal_en_primeras_tres = False
     else:
         # Adentro de una palabra
         contador += 1
@@ -23,6 +35,13 @@ for caracter in cadena:
         if contador == 1:
             primera_letra_de_palabra = caracter
 
+        if (caracter_anterior + caracter) == 'tr':
+            contiene_tr = True
+
+        if contador <= 3 and es_vocal(caracter):
+            incluye_vocal_en_primeras_tres = True
+
     caracter_anterior = caracter
 
 print(cantidad_palabras_empiezan_terminan_igual)
+print(cantidad_palabras_con_tr_y_vocal_primeras_3)
