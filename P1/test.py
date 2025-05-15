f8996b586ac49078da30e9857164c53c0d98cc71
igna_martinoli <ignamartinoli@protonmail.com>
2025-05-15 18:41:51 -0300

    update

--- P1/test.py
+++ P1/test.py
@@ -9,7 +9,9 @@ N = 10_000
 
 cantidad_numeros_entre_1_y_10000 = 0
 cantidad_numeros_entre_10000_y_20000 = 0
-cantidad_numeros_mayores_que_20000 = 0
+cantidad_numeros_mayores_a_20000 = 0
+
+cantidad_numeros_divisibles_por_6 = 0
 
 for i in range(N):
     x = random.randint(1, 25_000)
@@ -19,9 +21,14 @@ for i in range(N):
     if 10_000 <= x < 20_000:
         cantidad_numeros_entre_10000_y_20000 += 1
     if x >= 20_000:
-        cantidad_numeros_mayores_que_20000 += 1
+        cantidad_numeros_mayores_a_20000 += 1
+
+    if x % 6 == 0:
+        cantidad_numeros_divisibles_por_6 += 1
 
 
-print("Q 1, 10000:", cantidad_numeros_entre_1_y_10000)
-print("Q 1, 10000:", cantidad_numeros_entre_1_y_10000)
-print("Q 1, 10000:", cantidad_numeros_entre_1_y_10000)
+print("Q 1, E 1:", cantidad_numeros_entre_1_y_10000)
+print("Q 1, E 2:", cantidad_numeros_entre_10000_y_20000)
+print("Q 1, E 3:", cantidad_numeros_mayores_a_20000)
+
+print("Q 2:", cantidad_numeros_divisibles_por_6)
